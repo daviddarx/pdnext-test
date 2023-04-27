60c4261ef8f5e74fec23432b9dc9bdd78a19dc55
David Darx <david.darx@liip.ch>
2023-04-27 20:58:08 +0200

    feat(font): implement google font

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,7 +1,14 @@
 import { ReactNode } from 'react';
+import { Space_Grotesk } from 'next/font/google';
 
 import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
+
+const font = Space_Grotesk({
+  weight: ['400', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+});
 
 interface Props {
   children?: ReactNode;
@@ -9,7 +16,7 @@ interface Props {
 
 const Layout = ({ children }: Props) => {
   return (
-    <div className='layout'>
+    <div className={`layout ${font.className}`}>
       <Header />
       <main className='layout__page'>{children}</main>
       <Footer />
