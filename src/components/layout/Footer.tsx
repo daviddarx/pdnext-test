a35be0f4024b69031196948f92d3a2c92289262d
David Darx <david.darx@liip.ch>
2023-04-29 09:40:49 +0200

    feat(footer): refactor and lint

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,20 @@
 import ActiveLink from '@/components/ui/ActiveLink';
 
+const footerNavItems = [
+  {
+    title: 'Impressum',
+    link: '/impressum',
+  },
+  {
+    title: 'Datenschutz',
+    link: '/privacy',
+  },
+  {
+    title: 'Cookie-Richtlinie',
+    link: '/cookies',
+  },
+];
+
 const Footer = () => {
   const currentYear = new Date().getFullYear();
 
@@ -8,33 +23,17 @@ const Footer = () => {
       <nav>
         <h2 className='hidden'>Footer Navigation</h2>
         <ul className='footer__nav'>
-          <li>
-            <ActiveLink
-              className='footer__link'
-              activeClassName='footer__link--active'
-              href='/impressum'
-            >
-              Impressum
-            </ActiveLink>
-          </li>
-          <li>
-            <ActiveLink
-              className='footer__link'
-              activeClassName='footer__link--active'
-              href='/privacy'
-            >
-              Datenschutz
-            </ActiveLink>
-          </li>
-          <li>
-            <ActiveLink
-              className='footer__link'
-              activeClassName='footer__link--active'
-              href='/cookies'
-            >
-              Cookie-Richtlinie
-            </ActiveLink>
-          </li>
+          {footerNavItems.map((item) => (
+            <li key={item.link}>
+              <ActiveLink
+                className='footer__link'
+                activeClassName='footer__link--active'
+                href={item.link}
+              >
+                {item.title}
+              </ActiveLink>
+            </li>
+          ))}
         </ul>
       </nav>
       <p>©{currentYear} Porny Days</p>
