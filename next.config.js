83df9baf583063f3248e8edbd4d301151be38aae
David Darx <david.darx@liip.ch>
2023-05-02 20:07:45 +0200

    feat(next-config): update redirect without permanent

--- next.config.js
+++ next.config.js
@@ -1,15 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-const staticContent = require('./static-content/static-content.json');
-
 const nextConfig = {
   reactStrictMode: true,
   async redirects() {
     return [
       {
         source: '/',
-        destination: staticContent.defaultRoute,
-        permanent: true,
+        destination: '/festival-program',
+        permanent: false,
       },
     ];
   },
