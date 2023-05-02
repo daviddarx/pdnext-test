a783b05b9a23ba7b9805431a9d383f3f77a261cd
David Darx <david.darx@liip.ch>
2023-05-02 19:48:24 +0200

    feat(support-us): add component for heart icon

--- src/components/layout/SupportUs.tsx
+++ src/components/layout/SupportUs.tsx
@@ -1,6 +1,7 @@
 import ReactMarkdown from 'react-markdown';
 
 import { SupportUsSlot } from '@/types/SupportUsSlot';
+import IconHeart from '@/components/icons/IconHeart';
 
 type Props = {
   supportUsData: SupportUsSlot[];
@@ -13,15 +14,7 @@ const SupportUs = ({ supportUsData }: Props) => {
     <div className='support-us'>
       <h2 className='support-us__title '>
         Unterstützen Sie uns
-        <svg
-          className='support-us__icon'
-          xmlns='http://www.w3.org/2000/svg'
-          width='422.169'
-          height='364.685'
-          viewBox='0.5 600.5 422.169 364.685'
-        >
-          <path d='M111.825 600.5c-28.543 0-57.021 11.011-78.75 32.85-43.459 43.68-43.405 114.392 0 158.101l168.15 169.5c5.615 5.633 14.733 5.648 20.367.033l.033-.033c56.133-56.416 112.317-112.783 168.45-169.2 43.458-43.678 43.458-114.571 0-158.25-43.459-43.678-114.19-43.68-157.65 0l-20.7 21-21-21.15c-21.73-21.84-50.358-32.851-78.9-32.851z' />
-        </svg>
+        <IconHeart className='support-us__icon' />
       </h2>
 
       {supportUsData.map((item) => (
