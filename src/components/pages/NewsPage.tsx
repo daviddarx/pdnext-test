fac6801ba82f146f1404351370fdfe0975b7a974
David Darx <david.darx@liip.ch>
2023-05-06 20:27:38 +0200

    feat(news): add final content on the news page, update structure

--- src/components/pages/NewsPage.tsx
+++ src/components/pages/NewsPage.tsx
@@ -1,12 +1,60 @@
+import Image from 'next/image';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+
 import { NewsContent } from '@/utils/fetch-news-content';
+
+import PageHeader from '@/components/layout/PageHeader';
 
 type Props = {
   data: NewsContent;
 };
 
 const NewsPage: React.FC<Props> = ({ data }) => {
-  console.log(data);
-  return <div>Je suis un NewsPage</div>;
+  return (
+    <section className='news-page'>
+      <PageHeader subline='Porny News' title='Keinen Höhepunkt verpassen' />
+      <div className='news-page__news'>
+        {data.news.map((item) => (
+          <article key={item.date + item.title} className='news-item'>
+            {item.image && (
+              <div className='news-item__image'>
+                {item.link ? (
+                  <a href={item.link}>
+                    <Image
+                      src={item.image}
+                      alt={item.title}
+                      width={item.imageWidth}
+                      height={item.imageHeight}
+                    />
+                  </a>
+                ) : (
+                  <Image
+                    src={item.image}
+                    alt={item.title}
+                    width={item.imageWidth}
+                    height={item.imageHeight}
+                  />
+                )}
+              </div>
+            )}
+            <div className='news-item__content'>
+              <div className='news-item__date'>{item.dateReadable}</div>
+              <h2 className='news-item__title'>
+                {item.title} {item.imageWidth} {item.imageHeight}
+              </h2>
+              {item.desc && (
+                <ReactMarkdown className='news-item__desc' remarkPlugins={[remarkGfm]}>
+                  {item.desc}
+                </ReactMarkdown>
+              )}
+              {item.link && <a href={item.link}>{item.linkTitle ? item.linkTitle : 'Link'}</a>}
+            </div>
+          </article>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default NewsPage;
--- src/utils/fetch-news-content.ts
+++ src/utils/fetch-news-content.ts
@@ -1,14 +1,44 @@
+import sizeOf from 'image-size';
+import fs from 'fs';
+
 import loadJsonFiles from '@/utils/load-json-files';
 
-export interface NewsContent {
-  news: any[];
+interface News {
+  title: string;
+  date: string;
+  dateReadable: string;
+  image?: string;
+  imageWidth?: number;
+  imageHeight?: number;
+  desc?: string;
+  link?: string;
+  linkTitle?: string;
 }
+
+export interface NewsContent {
+  news: News[];
+}
+
 export async function fetchNewsContent(): Promise<NewsContent> {
-  // const newsDir: NewsType[] = [];
-  const newsDir: [] = [];
+  const newsDir: News[] = [];
   const news = await loadJsonFiles(newsDir, '_content/news');
 
-  // news.sort((a, b) => a.order - b.order);
+  for (const newItem of news) {
+    newItem.dateReadable = new Date(newItem.date).toLocaleDateString('de-DE', {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+    });
+
+    if (newItem.image) {
+      const imagePath = `public${newItem.image}`;
+      const dimensions = sizeOf(fs.readFileSync(imagePath));
+      newItem.imageWidth = dimensions.width;
+      newItem.imageHeight = dimensions.height;
+    }
+  }
+
+  news.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
 
   return {
     news: news,
