bb7a1026d755ae50d5c689ced1e7d1bed3273cae
David Darx <david.darx@liip.ch>
2023-05-07 19:13:22 +0200

    feat(special-announcement): add drawer

--- src/components/layout/SpecialAnnouncement.tsx
+++ src/components/layout/SpecialAnnouncement.tsx
@@ -1,15 +1,57 @@
+import { Fragment, useState, useEffect } from 'react';
+import { Portal } from 'react-portal';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+
 import { SpecialAnnouncement } from '@/types/SpecialAnnouncement';
+
+import Drawer from '@/components/ui/Drawer';
 
 type Props = {
   data: SpecialAnnouncement;
 };
 
 const SpecialAnnouncement: React.FC<Props> = ({ data }) => {
+  const [isOpened, setIsOpened] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const open = () => {
+    setIsOpened(true);
+  };
+
+  const close = () => {
+    setIsOpened(false);
+  };
+
   return (
-    <article className='special-announcement'>
-      <h2 className='special-announcement__title'>Special Announcement</h2>
-      <button className='special-announcement__btn'>{data.buttonTitle}</button>
-    </article>
+    <Fragment>
+      <article className='special-announcement'>
+        <h2 className='special-announcement__title'>Special Announcement</h2>
+        <button className='special-announcement__btn' onClick={open}>
+          {data.buttonTitle}
+        </button>
+      </article>
+      {isMounted && (
+        <Portal>
+          <Drawer isOpened={isOpened} onClose={close}>
+            <div className='special-announcement-panel'>
+              <span className='special-announcement-panel__subline'>Special Announcement</span>
+              <h2 className='special-announcement-panel__title'>{data.title}</h2>
+              <ReactMarkdown
+                className='special-announcement-panel__desc'
+                remarkPlugins={[remarkGfm]}
+              >
+                {data.desc}
+              </ReactMarkdown>
+            </div>
+          </Drawer>
+        </Portal>
+      )}
+    </Fragment>
   );
 };
 
