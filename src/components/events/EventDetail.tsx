dbaba0b00842a7eb4e3216d04cb55f50e416d8bb
David Darx <david.darx@liip.ch>
2023-05-09 07:46:21 +0200

    feat(event-detail): add animation

--- src/components/events/EventDetail.tsx
+++ src/components/events/EventDetail.tsx
@@ -1,5 +1,6 @@
 import { Fragment } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { AnimatePresence, motion } from 'framer-motion';
 
 import { uiActions } from '@/store/';
 import { uiStateType } from '@/store/ui-slice';
@@ -14,16 +15,23 @@ const EventDetail = () => {
   };
 
   return (
-    <Fragment>
+    <AnimatePresence mode='popLayout'>
       {openedEvent && (
-        <article className='event-detail'>
+        <motion.article
+          key={openedEvent.id}
+          className='event-detail'
+          initial={{ x: '100%' }}
+          animate={{ x: 0 }}
+          exit={{ x: '-100%' }}
+          transition={{ duration: 0.4 }}
+        >
           <h1>{openedEvent.title}</h1>
           <button className={'event-detail__close'} onClick={close}>
             Close
           </button>
-        </article>
+        </motion.article>
       )}
-    </Fragment>
+    </AnimatePresence>
   );
 };
 
