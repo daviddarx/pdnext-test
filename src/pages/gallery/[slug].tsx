6f25f1484fdc53d51c3507e9cbcd631774a63829
David Darx <david.darx@liip.ch>
2023-06-09 10:43:39 +0200

    feat(gallery): implement lightbox

--- src/pages/gallery/[slug].tsx
+++ src/pages/gallery/[slug].tsx
@@ -1,3 +1,9 @@
+import { useState } from 'react';
+import Lightbox from 'yet-another-react-lightbox';
+import Counter from 'yet-another-react-lightbox/plugins/counter';
+import 'yet-another-react-lightbox/styles.css';
+import 'yet-another-react-lightbox/plugins/counter.css';
+
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 
 import { fetchCommonPageContent, CommonPageData } from '@/utils/fetch-common-page-content';
@@ -22,6 +28,27 @@ type PageProps = {
 };
 
 const Page: NextPage<PageProps> = ({ page, commonPageData }) => {
+  const [isGalleryOpened, setGalleryOpened] = useState(false);
+  const [galleryIndex, setGalleryIndex] = useState(0);
+
+  const galleryImages = page.images.map((item) => {
+    return { src: item.main };
+  });
+
+  const openGallery = (e: React.MouseEvent, index: number) => {
+    const href = e.currentTarget.getAttribute('href');
+
+    if (href) {
+      setGalleryIndex(index);
+      setGalleryOpened(true);
+      e.preventDefault();
+    }
+  };
+
+  const closeGallery = () => {
+    setGalleryOpened(false);
+  };
+
   return (
     <Layout commonPageData={commonPageData}>
       <Metas title={page.title} />
@@ -30,8 +57,13 @@ const Page: NextPage<PageProps> = ({ page, commonPageData }) => {
         <PageHeader subline={page.subline} title={page.title} lead={page.lead} />
 
         <div className='gallery-page__images'>
-          {page.images.map((image) => (
-            <a href={image.main} key={image.main} className='gallery-page__link'>
+          {page.images.map((image, i) => (
+            <a
+              href={image.main}
+              key={image.main}
+              className='gallery-page__link'
+              onClick={(e) => openGallery(e, i)}
+            >
               <div className='gallery-page__image'>
                 <LoadedImageCustom src={image.thumb} alt={page.title} />
               </div>
@@ -39,6 +71,26 @@ const Page: NextPage<PageProps> = ({ page, commonPageData }) => {
           ))}
         </div>
       </section>
+
+      <Lightbox
+        open={isGalleryOpened}
+        close={closeGallery}
+        index={galleryIndex}
+        slides={galleryImages}
+        controller={{ closeOnBackdropClick: true }}
+        plugins={[Counter]}
+        counter={{ container: { className: 'test' } }}
+        animation={{
+          fade: 250,
+          swipe: 750,
+          navigation: 500,
+          easing: {
+            fade: 'ease',
+            swipe: 'cubic-bezier(0.230, 1.000, 0.320, 1.000)' /* outQuint */,
+            navigation: 'cubic-bezier(0.860, 0.000, 0.070, 1.000)' /* inOutQuint */,
+          },
+        }}
+      />
     </Layout>
   );
 };
