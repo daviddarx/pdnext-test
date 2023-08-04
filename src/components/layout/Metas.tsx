1c1030b1564f40b4c3d55d48d8ba0bb991f62cc9
David Darx <david.darx@liip.ch>
2023-08-04 16:47:49 +0200

    fix(metas): add description

--- src/components/layout/Metas.tsx
+++ src/components/layout/Metas.tsx
@@ -6,16 +6,16 @@ interface Props {
 
 const Metas = ({ title }: Props) => {
   const combinedTitle = `10. Porny Days / 23. bis 27. Nov. 2022 / Zürich – ${title}`;
+  const description =
+    'Porny Days ist ein Film Kunst Festival in Zürich: Filme, Debatten, Performances &amp; Party – von Stummfilm bis Postporn.';
 
   return (
     <Head>
       <title>{combinedTitle}</title>
-      <meta
-        property='og:description'
-        content='Porny Days ist ein Film Kunst Festival in Zürich: Filme, Debatten, Performances &amp; Party – von Stummfilm bis Postporn.'
-      />
+      <meta name='description' content={description} />
+      <meta property='og:description' content={description} />
       <meta property='og:url' content='https://www.pornydays.love' />
-      <meta property='og:title' content='10. Porny Days / 23. bis 27. Nov. 2022 / Zürich' />
+      <meta property='og:title' content={combinedTitle} />
       <meta property='og:image:url' content='/images/pornydays.jpg' />
       <meta property='og:image:secure_url' content='/images/pornydays.jpg' />
       <meta property='og:image:type' content='image/jpeg' />
