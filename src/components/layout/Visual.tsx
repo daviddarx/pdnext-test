4b192534514a4d72c5e68959bf36ca26e652147c
David Darx <david.darx@liip.ch>
2023-10-01 13:09:28 +0200

    feat(visual): implement

--- src/components/layout/Visual.tsx
+++ src/components/layout/Visual.tsx
@@ -1,8 +1,30 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import { debounce } from 'lodash-es';
+import classNames from 'classnames';
 
 const Visual = () => {
+  const [faded, setFaded] = useState(false);
+
+  useEffect(() => {
+    function onScroll() {
+      if (window.scrollY > 200) {
+        setFaded(true);
+      } else {
+        setFaded(false);
+      }
+    }
+
+    const onScrollDebounced = debounce(onScroll, 20);
+
+    document.addEventListener('scroll', onScrollDebounced, { passive: true });
+
+    return () => {
+      document.removeEventListener('scroll', onScrollDebounced);
+    };
+  });
+
   return (
-    <div className='visual'>
+    <div className={classNames('visual', { faded: faded })}>
       <video autoPlay muted loop playsInline className='visual__video'>
         <source src='https://files.daviddarx.com/pornydays/porny_bg_video.mp4' type='video/mp4' />
       </video>
