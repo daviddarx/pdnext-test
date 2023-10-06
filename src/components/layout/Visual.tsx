c32457c553b163488111033b2ae2951a02b05dbb
David Darx <david.darx@liip.ch>
2023-10-06 13:36:44 +0200

    feat(visual): reset 3d visual, clean video

--- src/components/layout/Visual.tsx
+++ src/components/layout/Visual.tsx
@@ -1,30 +1,8 @@
-import React, { useRef, useEffect, useState } from 'react';
-import { debounce } from 'lodash-es';
-import classNames from 'classnames';
+import React from 'react';
 
 const Visual = () => {
-  const [faded, setFaded] = useState(false);
-
-  useEffect(() => {
-    function onScroll() {
-      if (window.scrollY > 200) {
-        setFaded(true);
-      } else {
-        setFaded(false);
-      }
-    }
-
-    const onScrollDebounced = debounce(onScroll, 20);
-
-    document.addEventListener('scroll', onScrollDebounced, { passive: true });
-
-    return () => {
-      document.removeEventListener('scroll', onScrollDebounced);
-    };
-  });
-
   return (
-    <div className={classNames('visual', { faded: faded })}>
+    <div className='visual'>
       <video autoPlay muted loop playsInline className='visual__video'>
         <source src='https://files.daviddarx.com/pornydays/porny_bg_video.mp4' type='video/mp4' />
       </video>
