c32b69792f5efefafe86ccf1a5aedce25b6c0af4
David Darx <daviddarx@gmail.com>
2024-10-27 10:58:43 +0100

    feat(visual): reimplement faded on scroll for mobile

--- src/components/layout/Visual.tsx
+++ src/components/layout/Visual.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import classNames from 'classnames';
 import { uiStateType } from '@/store/ui-slice';
+import { debounce } from 'lodash-es';
 
 const videosNames = [
   'porny_2024-01',
@@ -14,18 +15,36 @@ const videosNames = [
 const videosURL = 'https://files.daviddarx.com/pornydays/videos/2024/';
 
 const Visual = () => {
+  const [mounted, setMounted] = useState(false);
+  const [faded, setFaded] = useState(false);
   const isDark = useSelector((state: uiStateType) => state.ui.isDark);
   const isContentPage = useSelector((state: uiStateType) => state.ui.isContentPage);
   const [videoURL, setVideoURL] = useState('');
 
   useEffect(() => {
     setVideoURL(videosNames[Math.floor(Math.random() * videosNames.length)]);
-  }, [setVideoURL]);
+    setMounted(true);
+  }, [setVideoURL, setMounted]);
+
+  useEffect(() => {
+    const onScroll = () => {
+      setFaded(window.scrollY > window.innerHeight * 0.75);
+    };
+    const onScrollDebounced = debounce(onScroll, 20);
+    if (mounted) {
+      document.addEventListener('scroll', onScrollDebounced, { passive: true });
+    }
+
+    return () => {
+      document.removeEventListener('scroll', onScrollDebounced);
+    };
+  }, [mounted]);
 
   return (
     <div
       className={classNames('visual', {
         'visual--dark': isDark,
+        'visual--fadedout': faded,
         'visual--content-page': isContentPage && !isDark,
       })}
     >
